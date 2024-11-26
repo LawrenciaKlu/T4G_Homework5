5355f9e0f4998634bc385219d05109b2008ac049
LawrenciaKlu <you@example.com>
2024-11-26 08:18:27 -0800

    adding all changes

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let name = prompt("Enter your name")
+let age = prompt("Enter your age")
+let email = prompt("Enter your email")
+
+alert(`Hi ${name}, you are ${age} years old and we will keep in touch via your email ${email}👋🏼👋🏼`)
